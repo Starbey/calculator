c592d0a148c7502e9950ad9da10faee8e5e00ea9
BenGavieres <benjamin.gavieres@gmail.com>
2023-07-14 14:50:11 -0400

    Change to number buttons: assign in a for-loop.

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 //buttons
-const btnAc=document.getElementById("ac");
+const btnAC=document.getElementById("ac");
 const btnDel=document.getElementById("del");
+
 const btnPercent=document.getElementById("percent");
 const btnAdd=document.getElementById("add");
-const btnSeven=document.getElementById("7");
-const btnEight=document.getElementById("8");
-const btnNine=document.getElementById("9");
 const btnMultiply=document.getElementById("multiply");
-const btnFour=document.getElementById("4");
-const btnFive=document.getElementById("5");
-const btnSix=document.getElementById("6");
 const btnDivide=document.getElementById("divide");
-const btnOne=document.getElementById("1");
-const btnTwo=document.getElementById("2");
-const btnThree=document.getElementById("3");
 const btnSubtract=document.getElementById("subtract");
-const btnZero=document.getElementById("0");
-const btnDecimal=document.getElementById("decimal");
 const btnEquals=document.getElementById("equals");
+const btnDecimal=document.getElementById("decimal");
+
+// const btnSeven=document.getElementById("7");
+// const btnEight=document.getElementById("8");
+// const btnNine=document.getElementById("9");
+// const btnFour=document.getElementById("4");
+// const btnFive=document.getElementById("5");
+// const btnSix=document.getElementById("6");
+// const btnOne=document.getElementById("1");
+// const btnTwo=document.getElementById("2");
+// const btnThree=document.getElementById("3");
+// const btnZero=document.getElementById("0");
+
+const numButtons =[];
+for (let i=0;i<9;i++){
+    numButtons[i]=document.getElementById(`${i}`);
+}
 
 function add(numOne,numTwo){
     return numOne+numTwo;
@@ -53,4 +60,6 @@ function operate(numOne,operator,numTwo) {
             console.log("Invalid operator");
     }
 }
+
+
 
